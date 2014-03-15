fab5da4b9c04258ff26c6242d83bebd844706bbb
Johan Stenehall <stenehall@gmail.com>
2014-03-15 22:44:43 +0100

    Removing console.log

--- server/main/index.js
+++ server/main/index.js
@@ -45,9 +45,6 @@ app.configure(function() {
 });
 
 app.get('/', auth.ensureAuthenticatedUser, function(req, res) {
-
-  console.log(req.session);
-
   res.render('index.ejs', {title: "En title"});
 });
 
