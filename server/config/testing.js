2d3c7be00cf4ac86c03f134d1956961cea00f3c8
Johan Stenehall <stenehall@gmail.com>
2014-03-17 23:31:50 +0100

    Working tests

---
+++ server/config/testing.js
@@ -0,0 +1,13 @@
+module.exports = {
+  'parse': {
+    'appId': 'foo',
+    'jsKey': 'bar'
+  },
+  'auth': {
+    'github': {
+      'clientID': 'foo',
+      'clientSecret': 'bar',
+      'ids': ['123']
+    }
+  },
+};
---
+++ server/main/test/main_test.js
@@ -0,0 +1,46 @@
+var assert = require('chai').assert,
+    request = require('supertest'),
+    nock = require('nock'),
+    config = require('../../config');
+
+var app;
+
+
+
+describe('Make sure config works', function() {
+  it("should have all needed settings in config");
+});
+
+
+describe('Main unauthorized', function(){
+
+  beforeEach(function() {
+    // Since we're adding things to our app we need to make sure we have an uncache version
+    delete require.cache[require.resolve('../index')];
+    app = require('../index').app;
+  });
+
+  it("should redirect to /auth/login if not authorized when trying to access /", function() {
+    request(app)
+    .get('/')
+    .expect(302)
+    .end(function(err, res){
+      if (err) throw err;
+    });
+  });
+
+});
+
+describe('Main authorized', function(){
+  it("should let you view / if authorized ");
+});
+
+
+
+
+
+
+
+describe('Make sure config works', function() {
+  it("should have all needed settings in config");
+});
